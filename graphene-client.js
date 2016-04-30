3eddd38488afae0388e874858b65904c822e3778
Marco Magnetti <marcomagnetti@gmail.com>
2016-04-30 16:22:13 +0200

    Security and feature updates

---
+++ graphene-client.js
@@ -0,0 +1,10 @@
+(function() {
+    'use strict';
+    Polymer({
+        is:        'graphene-client',
+        behaviors: [
+            Graphene.behaviors.clientBehavior
+        ]
+    });
+})();
+
